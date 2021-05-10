fd9cabc0a86fa787fadad7e2a7faab4d75741bec
wnourse05 <wrn13@case.edu>
2021-05-10 09:27:34 -0400

    Added non-spiking neuron class. Type checking should be added at some point

--- sns-toolbox/design/neurons.py
+++ sns-toolbox/design/neurons.py
@@ -21,7 +21,12 @@ BASE CLASS
 """
 
 class Neuron:
-    def __init__(self, name: str = 'Neuron', color: str = 'white'):
+    def __init__(self, name: str = 'Neuron', color: str = 'white') -> None:
+        """
+        Constructor for base Neuron class
+        :param name:    Name of this neuron preset
+        :param color:   Background fill color for the neuron, taken from the standard SVG colors
+        """
         self.params: Dict[str, Any] = {}
         if validColor(color):
             self.params['color'] = color
@@ -35,5 +40,18 @@ class Neuron:
 SPECIFIC MODELS
 """
 
-# Only one of these (for now, or for forever *shrugs*
+# Only one of these (for now, or for forever *shrugs*)
 
+class NonSpikingNeuron(Neuron):
+    def __init__(self, membraneCapacitance: float = 5.0, membraneConductance: float = 1.0, bias: float = 0.0) -> None:
+        """
+        Classic non-spiking neuron model, whose dynamics are as follows:
+        membraneCapacitance*dU/dt = -membraneConductance*U + bias + synaptic + external
+        :param membraneCapacitance: (nF)
+        :param membraneConductance: (uS)
+        :param bias:                Bias current (nA)
+        """
+        super().__init__()
+        self.params['membraneCapacitance'] = membraneCapacitance
+        self.params['membraneConductance'] = membraneConductance
+        self.params['bias'] = bias
