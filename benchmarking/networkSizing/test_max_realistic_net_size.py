ec106a480fd64f7da826b5101dc6f9b229ae6ad8
wnourse05 <wrn13@case.edu>
2022-03-14 15:23:33 -0400

    Testing for LM paper

--- benchmarking/networkSizing/test_max_realistic_net_size.py
+++ benchmarking/networkSizing/test_max_realistic_net_size.py
@@ -3,67 +3,54 @@ import torch
 import time
 
 from sns_toolbox.design.networks import Network
-from sns_toolbox.design.neurons import NonSpikingNeuron
-from sns_toolbox.design.connections import NonSpikingSynapse
+from sns_toolbox.design.neurons import SpikingNeuron
+from sns_toolbox.design.connections import SpikingSynapse
 
 from sns_toolbox.simulate import backends
 
-backend = 0 # 0: manual
+backend = 1 # 0: manual
             # 1: Numpy
             # 2: Torch
             # 3: Sparse
 cpu = True
-variant = 0 # 0: full model
-            # 1: No delay
-            # 2: non-spiking
 
 theoretical_max = 200000
 start_size = 10
 start_time = time.time()
 def design_network(size):
     print('Designing. %i sec elapsed'%(time.time()-start_time))
-    neuron_type = NonSpikingNeuron()
-    synapse_type = NonSpikingSynapse()
     net = Network()
-    net.add_population(neuron_type,[size])
-    net.add_connection(synapse_type,0,0)
-    net.add_input(0)
+    numIns = int(0.08 * size) + 1
+    numOuts = int(0.12 * size)
+    numSyn = int(np.sqrt(size))
+    numRest = int(size) - numIns - numSyn - numOuts
+    current = 10.0
+    spike = SpikingNeuron(name='m<0', threshold_proportionality_constant=-1, color='aquamarine')
+    spikeBias = SpikingNeuron(name='bias', threshold_proportionality_constant=-1, color='aquamarine', bias=current)
+    spikeExcite = SpikingSynapse(name='Excitatory Spiking')
+    net.add_population(spike, [numIns], name='ins')  # in puppy, num_inputs = 8% of network
+    net.add_population(spikeBias, [numOuts], name='outs')  # in puppy, num_outputs = 12% of network
+    net.add_population(spikeBias, [numSyn], name='connected')  # in puppy, numSyn = shape
+    net.add_population(spikeBias, [numRest], name='rest')  # rest of the network
+    net.add_connection(spikeExcite, 'connected', 'connected')
+    net.add_input('ins')
+    net.add_output('outs')
     return net
 
-def build_network(net,backend,cpu,variant):
+def build_network(net,backend,cpu):
     print('Building. %i sec elapsed'%(time.time()-start_time))
     if cpu:
         device = 'cpu'
     else:
         device = 'cuda'
     if backend == 0:
-        if variant == 0:
-            model = backends.SNS_Manual(net)
-        elif variant == 1:
-            model = backends.SNS_Manual(net,delay=False)
-        else:
-            model = backends.SNS_Manual(net,spiking=False)
+        model = backends.SNS_Manual(net)
     elif backend == 1:
-        if variant == 0:
-            model = backends.SNS_Numpy(net)
-        elif variant == 1:
-            model = backends.SNS_Numpy(net,delay=False)
-        else:
-            model = backends.SNS_Numpy(net,spiking=False)
+        model = backends.SNS_Numpy(net)
     elif backend == 2:
-        if variant == 0:
-            model = backends.SNS_Torch(net,device=device)
-        elif variant == 1:
-            model = backends.SNS_Torch(net,device=device,delay=False)
-        else:
-            model = backends.SNS_Torch(net,device=device,spiking=False)
+        model = backends.SNS_Torch(net,device=device)
     else:
-        if variant == 0:
-            model = backends.SNS_Sparse(net,device=device)
-        elif variant == 1:
-            model = backends.SNS_Sparse(net,device=device,delay=False)
-        else:
-            model = backends.SNS_Sparse(net,device=device,spiking=False)
+        model = backends.SNS_Sparse(net,device=device)
     return model, device
 
 def run_network(model,backend,device):
@@ -74,12 +61,12 @@ def run_network(model,backend,device):
         inp = np.array([1.0])
     out = model.forward(inp)
 
-def process(size,backend,cpu,variant):
+def process(size,backend,cpu):
     net = design_network(size)
-    model, device = build_network(net,backend,cpu,variant)
+    model, device = build_network(net,backend,cpu)
     run_network(model,backend,device)
 
-def main_loop(start_size,max_size,backend,cpu,variant):
+def main_loop(start_size,max_size,backend,cpu):
     sizes = np.linspace(start_size,max_size,num=4).astype(int)
     i = 0
     failed = False
@@ -89,7 +76,7 @@ def main_loop(start_size,max_size,backend,cpu,variant):
         else:
             print(sizes[i])
             try:
-                process(sizes[i],backend,cpu,variant)
+                process(sizes[i],backend,cpu)
             except:
                 failed = True
         i += 1
@@ -101,17 +88,17 @@ def main_loop(start_size,max_size,backend,cpu,variant):
             print('All Done. Largest Network Size is %i neurons. %i elapsed sec'%(new_start,time.time()-start_time))
         else:
             print('Failed at size %i, trying again in new range. %i elapsed sec' % (sizes[i - 1],time.time()-start_time))
-            main_loop(new_start,new_max,backend,cpu,variant)
+            main_loop(new_start,new_max,backend,cpu)
     else:
         print('All Done. Largest Network Size is %i neurons. %i elapsed sec'%(max_size,time.time()-start_time))
 
-def main_loop_increment(start_size,max_size,backend,cpu,variant):
+def main_loop_increment(start_size,max_size,backend,cpu):
     size = start_size
     failed = False
-    while (not failed) and (size < max_size):
+    while (not failed) and (size <= max_size):
         print(size)
         try:
-            process(size,backend,cpu,variant)
+            process(size,backend,cpu)
         except:
             failed = True
         size += 500
@@ -120,5 +107,5 @@ def main_loop_increment(start_size,max_size,backend,cpu,variant):
     else:
         print('All Done. Largest Network Size is %i neurons. %i elapsed sec'%(max_size,time.time()-start_time))
 
-#main_loop(start_size,theoretical_max,backend,cpu,variant)
-main_loop_increment(start_size,theoretical_max+10,backend,cpu,variant)
+# main_loop(start_size,theoretical_max,backend,cpu)
+main_loop_increment(start_size,theoretical_max+10,backend,cpu)
