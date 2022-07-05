f8c7a7099ec3a6bfda47d36fee548aeb33d4ebbe
wnourse05 <wrn13@case.edu>
2022-07-05 15:12:40 -0400

    Switching from autodoc to autoapi

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../sns_toolbox/'))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('../sns_toolbox/'))
 
 
 # -- Project information -----------------------------------------------------
@@ -30,9 +30,10 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser'
+extensions = ['myst_parser', 'autoapi.extension'
 ]
 
+autoapi_dirs = ['../sns_toolbox']
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
