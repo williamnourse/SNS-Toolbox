b57f963e074a0c2d6c90212b2c2c92461bb8e74a
wnourse05 <wrn13@case.edu>
2022-07-12 16:18:45 -0400

    Sim_utilities docstrings

--- sns_toolbox/simulate/simulate_utilities.py
+++ sns_toolbox/simulate/simulate_utilities.py
@@ -1,14 +1,21 @@
 """
-Utility functions used when creating/simulating the various backends
-William Nourse
-September 13, 2021
-You called the guy 'Snot'?
+Various Utility functions used when creating/simulating the various backends.
 """
 
 import torch
 
 
 def send_vars(variables, device):
+    """
+    Send a set of variables to a specific device. Used with PyTorch backends.
+
+    :param variables:   Variables to send.
+    :type variables:    List of torch.tensors
+    :param device:      Device to send the variables to.
+    :type device:       torch.device
+    :return:            Variables sent to correct device.
+    :rtype:             List of torch.tensors
+    """
     for var in variables:
         var = var.to(device)
     torch.cuda.empty_cache()
