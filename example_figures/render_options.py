27e9a5eb421f6f69b653cda0e84c58529ca8d7f8
wnourse05 <wrn13@case.edu>
2022-08-11 12:44:57 -0400

    Render support for populations

--- example_figures/render_options.py
+++ example_figures/render_options.py
@@ -1,6 +1,10 @@
+"""
+Showcase the features of the render engine.
+"""
 from sns_toolbox.neurons import SpikingNeuron
 from sns_toolbox.connections import NonSpikingSynapse, NonSpikingPatternConnection
 from sns_toolbox.networks import Network
+from sns_toolbox.renderer import render
 
 import numpy as np
 
@@ -13,7 +17,7 @@ synapse_modulatory = NonSpikingSynapse(relative_reversal_potential=0.0)
 kernel_1d = np.array([-1,1,-1])
 synapse_pattern = NonSpikingPatternConnection(kernel_1d+2,kernel_1d)
 
-net = Network(name='All Elements')
+net = Network(name='Render')
 net.add_neuron(neuron_type,name='Single Neuron')
 net.add_population(neuron_type,shape=[5],name='1D Population')
 net.add_population(neuron_type,shape=[2,2],name='2D Population')
@@ -32,4 +36,4 @@ net.add_connection(synapse_inhibitory,0,2)
 net.add_connection(synapse_modulatory,2,0)
 net.add_connection(synapse_pattern,1,3)
 
-net.render_graph(view=True,imgFormat='svg')
+render(net, view=True, save=False, filename='Test')
---
+++ sns_toolbox/renderer.py
@@ -0,0 +1,60 @@
+"""
+Take in a network, and render its structure using the package 'graphviz'.
+"""
+from sns_toolbox.networks import Network
+from sns_toolbox.color_utilities import set_text_color
+
+import warnings
+
+graphviz_available = True
+try:    # see if graphviz is available
+    from graphviz import Digraph
+except ImportError:
+    warnings.warn('Warning: graphviz package not found, network rendering is disabled')
+    graphviz_available = False
+
+try:    # check if we're in an ipython environment
+    shell = get_ipython().__class__.__name__
+    in_ipython = True
+except NameError:
+    in_ipython = False
+
+def render(net: Network, view=True, save=False, filename=None, img_format='png'):
+    if not isinstance(net, Network):
+        raise TypeError('Invalid Network object')
+    if filename is None:
+        filename = net.params['name']
+    if graphviz_available:
+        graph = Digraph(filename=filename)
+        """Add the populations"""
+        for i in range(len(net.populations)):
+            pop = net.populations[i]
+            color_cell = pop['color']   # get cell color
+            pop_size = pop['number']    # get pop size
+            name = pop['name']  # get cell name
+            color_font = set_text_color(color_cell) # calc font color
+            pop_shape = pop['shape']
+            # set node shape
+            if pop_size > 1:    # population has multiple neurons
+                style = 'filled, rounded'
+                if len(pop_shape) > 1:
+                    shape = 'box3d' # population is 2d
+                else:
+                    shape = 'rect'
+            else:   # population is 1 neuron
+                shape = 'ellipse'
+                style = 'filled'
+
+            graph.node(str(i), label=name, style=style, shape=shape, fillcolor=color_cell, fontcolor=color_font)
+
+        # Display the network graph
+        graph.format = img_format
+        if save:
+            graph.render(view=False,cleanup=True)
+        if view:
+            if in_ipython:
+                return graph
+            else:
+                graph.render(view=True, cleanup=True)
+    else:
+        warnings.warn('Warning: graphviz package not found, network rendering is disabled')
