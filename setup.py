4d084ed326c3a1d07bc1c32ecd2bf18015da0d32
wnourse05 <wrn13@case.edu>
2022-12-15 12:35:52 -0500

    Updated setup to v1.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme_file_contents():
 
 setup(
     name='sns_toolbox',
-    version='1.3.1',
+    version='1.4.0',
     packages=find_packages(),
     url='https://github.com/wnourse05/SNS-Toolbox',
     license='Apache v2.0',
