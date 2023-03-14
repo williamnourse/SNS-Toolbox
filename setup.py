df1bc706aa984471c96b3ee560371ad26b6a400e
wnourse05 <wrn13@case.edu>
2023-03-14 16:45:17 -0400

    Updated version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme_file_contents():
 
 setup(
     name='sns_toolbox',
-    version='1.4.2',
+    version='1.4.3',
     packages=find_packages(),
     url='https://github.com/wnourse05/SNS-Toolbox',
     license='Apache v2.0',
